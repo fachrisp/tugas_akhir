9fb4fbd389958c48f14c6d8d4dabe9c7a4654366
fachrisp <fachrisatrya@gmail.com>
2018-08-05 23:34:11 +0700

    api.py

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ def home():
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
-@app.route('/api/resources/rfid/all', methods=['GET'])
+@app.route('/api/resources/alldata', methods=['GET'])
 def api_all():
     cnx = mysql.connector.connect(user='fachri', password='kinsl4y3r',
                               host='85.10.205.173',
@@ -45,46 +45,11 @@ def api_all():
     # import pdb
     # pdb.set_trace()
 
-
-
     return jsonify(return_obj)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_filter():
-    query_parameters = request.args
-
-    id = query_parameters.get('id')
-    published = query_parameters.get('published')
-    author = query_parameters.get('author')
-
-    query = "SELECT * FROM books WHERE"
-    to_filter = []
-
-    if id:
-        query += ' id=? AND'
-        to_filter.append(id)
-    if published:
-        query += ' published=? AND'
-        to_filter.append(published)
-    if author:
-        query += ' author=? AND'
-        to_filter.append(author)
-    if not (id or published or author):
-        return page_not_found(404)
-
-    query = query[:-4] + ';'
-
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-
-    results = cur.execute(query, to_filter).fetchall()
-
-    return jsonify(results)
 
 if __name__ == '__main__':
     #app.run(debug=True, use_reloader=True)
